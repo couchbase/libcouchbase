1b12b5463597dce6f9986621da53ee6b4e0e80ff
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-01-23 08:07:33 +0300

    Backport rpm fix from libvbucket
    See http://review.couchbase.org/12574
    
    Change-Id: I715af3d8a6b1e89b6aba9205462b0988f95f6269
    Reviewed-on: http://review.couchbase.org/12592
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- packaging/rpm/package.mk
+++ packaging/rpm/package.mk
@@ -5,6 +5,13 @@ RPM_DIR=$(RPM_WORKSPACE)/rpmbuild
 RPM_VER=$(shell echo $(VERSION) | awk -F_ '{ print $$1 }')
 RPM_REL=$(shell echo $(VERSION) | awk -F_ '{ print $$2"_"$$3 }')
 
+ifeq ($(RPM_REL),_)
+  RPM_REL=1
+  TARPREFIX=%{name}-%{version}
+else
+  TARPREFIX=%{name}-%{version}_%{release}
+endif
+
 dist-rpm: dist
 	rm -rf $(RPM_WORKSPACE)
 	mkdir -p $(RPM_DIR)
@@ -13,7 +20,7 @@ dist-rpm: dist
 	mkdir $(RPM_DIR)/RPMS
 	mkdir $(RPM_DIR)/SRPMS
 	cp $(PACKAGE)-$(VERSION).tar.gz $(RPM_DIR)/SOURCES
-	sed 's/@VERSION@/$(RPM_VER)/g;s/@RELEASE@/$(RPM_REL)/g' < packaging/rpm/$(PACKAGE).spec.in > $(RPM_WORKSPACE)/$(PACKAGE).spec
+	sed 's/@VERSION@/$(RPM_VER)/g;s/@RELEASE@/$(RPM_REL)/g;s/@TARREDAS@/$(TARPREFIX)/g' < packaging/rpm/$(PACKAGE).spec.in > $(RPM_WORKSPACE)/$(PACKAGE).spec
 	(cd $(RPM_WORKSPACE); rpmbuild -ba $(PACKAGE).spec)
 	mv $(RPM_DIR)/RPMS/*/*.rpm `pwd`
 	mv $(RPM_DIR)/SRPMS/*.rpm `pwd`
