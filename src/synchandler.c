efd0f61e3cfab48b04fcfbe6f2d6c75b2dbf73f2
Trond Norbye <trond.norbye@gmail.com>
2012-01-25 13:00:49 +0100

    Fix the sync mode and add a unit test
    Change-Id: I1719d6909295de3e7af38912e0feadfa9962b0e7
    Reviewed-on: http://review.couchbase.org/12661
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- src/synchandler.c
+++ src/synchandler.c
@@ -16,41 +16,292 @@
  */
 #include "internal.h"
 
+// @todo add static prototypes
+
 struct user_cookie {
     void *cookie;
-    libcouchbase_error_callback error;
+    struct libcouchbase_callback_st callbacks;
     libcouchbase_error_t retcode;
 };
 
+static void restore_user_env(libcouchbase_t instance);
+static void restore_wrapping_env(libcouchbase_t instance,
+                                 struct user_cookie *user,
+                                 libcouchbase_error_t error);
+
+static void tap_mutation_callback(libcouchbase_t instance,
+                                  const void *cookie,
+                                  const void *key,
+                                  libcouchbase_size_t nkey,
+                                  const void *data,
+                                  libcouchbase_size_t nbytes,
+                                  libcouchbase_uint32_t flags,
+                                  libcouchbase_time_t exp,
+                                  libcouchbase_cas_t cas,
+                                  libcouchbase_vbucket_t vbucket,
+                                  const void *es,
+                                  libcouchbase_size_t nes)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.tap_mutation(instance, cookie, key, nkey, data,
+                              nbytes, flags, exp, cas, vbucket,
+                              es, nes);
+    restore_wrapping_env(instance, c, LIBCOUCHBASE_SUCCESS);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void tap_deletion_callback(libcouchbase_t instance,
+                                  const void *cookie,
+                                  const void *key,
+                                  libcouchbase_size_t nkey,
+                                  libcouchbase_cas_t cas,
+                                  libcouchbase_vbucket_t vbucket,
+                                  const void *es,
+                                  libcouchbase_size_t nes)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.tap_deletion(instance, cookie, key, nkey,
+                              cas, vbucket, es, nes);
+    restore_wrapping_env(instance, c, LIBCOUCHBASE_SUCCESS);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void tap_flush_callback(libcouchbase_t instance,
+                               const void *cookie,
+                               const void *es,
+                               libcouchbase_size_t nes)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.tap_flush(instance, cookie, es, nes);
+    restore_wrapping_env(instance, c, LIBCOUCHBASE_SUCCESS);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void tap_opaque_callback(libcouchbase_t instance,
+                                const void *cookie,
+                                const void *es,
+                                libcouchbase_size_t nes)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.tap_opaque(instance, cookie, es, nes);
+    restore_wrapping_env(instance, c, LIBCOUCHBASE_SUCCESS);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void tap_vbucket_set_callback(libcouchbase_t instance,
+                                     const void *cookie,
+                                     libcouchbase_vbucket_t vbid,
+                                     libcouchbase_vbucket_state_t state,
+                                     const void *es,
+                                     libcouchbase_size_t nes)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.tap_vbucket_set(instance, cookie, vbid, state, es, nes);
+    restore_wrapping_env(instance, c, LIBCOUCHBASE_SUCCESS);
+    libcouchbase_maybe_breakout(instance);
+}
 
 static void error_callback(libcouchbase_t instance,
                            libcouchbase_error_t error,
                            const char *errinfo)
 {
-    struct user_cookie *cookie = (void*)instance->cookie;
-    if (error == LIBCOUCHBASE_SUCCESS) {
-        return ;
-    }
+    struct user_cookie *c = (void*)instance->cookie;
 
-    /* Restore the users environment */
-    instance->cookie = cookie->cookie;
-    instance->callbacks.error = cookie->error;
-
-    /* Call the user's callback */
-    cookie->error(instance, error, errinfo);
-
-    /* Restore the wrapping environment */
-    cookie->error = instance->callbacks.error; /* User might have changed this */
-    instance->cookie = cookie;
-
-    /* Save the error code */
-    cookie->retcode = error;
-
-    /* Ok, stop the event loop */
-    instance->io->stop_event_loop(instance->io);
+    restore_user_env(instance);
+    c->callbacks.error(instance, error, errinfo);
+    restore_wrapping_env(instance, c, error);
+    libcouchbase_maybe_breakout(instance);
 }
 
-libcouchbase_error_t libcouchbase_synchandler_return(libcouchbase_t instance, libcouchbase_error_t retcode)
+static void stat_callback(libcouchbase_t instance,
+                          const void* command_cookie,
+                          const char* server_endpoint,
+                          libcouchbase_error_t error,
+                          const void* key,
+                          libcouchbase_size_t nkey,
+                          const void* value,
+                          libcouchbase_size_t nvalue)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.stat(instance, command_cookie, server_endpoint,
+                      error, key, nkey, value, nvalue);
+    restore_wrapping_env(instance, c, error);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void get_callback(libcouchbase_t instance,
+                         const void *cookie,
+                         libcouchbase_error_t error,
+                         const void *key, libcouchbase_size_t nkey,
+                         const void *bytes, libcouchbase_size_t nbytes,
+                         libcouchbase_uint32_t flags, libcouchbase_cas_t cas)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.get(instance, cookie, error, key, nkey, bytes, nbytes,
+                     flags, cas);
+    restore_wrapping_env(instance, c, error);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void storage_callback(libcouchbase_t instance,
+                             const void *cookie,
+                             libcouchbase_storage_t operation,
+                             libcouchbase_error_t error,
+                             const void *key, libcouchbase_size_t nkey,
+                             libcouchbase_cas_t cas)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.storage(instance, cookie, operation, error, key,
+                         nkey, cas);
+    restore_wrapping_env(instance, c, error);
+
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void arithmetic_callback(libcouchbase_t instance,
+                                const void *cookie,
+                                libcouchbase_error_t error,
+                                const void *key, libcouchbase_size_t nkey,
+                                libcouchbase_uint64_t value,
+                                libcouchbase_cas_t cas)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.arithmetic(instance, cookie, error, key, nkey,
+                            value, cas);
+    restore_wrapping_env(instance, c, error);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void remove_callback(libcouchbase_t instance,
+                            const void *cookie,
+                            libcouchbase_error_t error,
+                            const void *key, libcouchbase_size_t nkey)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.remove(instance, cookie, error, key, nkey);
+    restore_wrapping_env(instance, c, error);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void touch_callback(libcouchbase_t instance,
+                           const void *cookie,
+                           libcouchbase_error_t error,
+                           const void *key, libcouchbase_size_t nkey)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.touch(instance, cookie, error, key, nkey);
+    restore_wrapping_env(instance, c, error);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void doc_complete_callback(libcouchbase_t instance,
+                                  const void *cookie,
+                                  libcouchbase_error_t error,
+                                  libcouchbase_http_status_t status,
+                                  const char *uri,
+                                  const void *bytes,
+                                  libcouchbase_size_t nbytes)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.doc_complete(instance, cookie, error, status,
+                              uri, bytes, nbytes);
+    restore_wrapping_env(instance, c, error);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void doc_data_callback(libcouchbase_t instance,
+                              const void *cookie,
+                              libcouchbase_error_t error,
+                              libcouchbase_http_status_t status,
+                              const char *uri,
+                              const void *bytes,
+                              libcouchbase_size_t nbytes)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.doc_data(instance, cookie, error, status,
+                          uri, bytes, nbytes);
+    restore_wrapping_env(instance, c, error);
+    libcouchbase_maybe_breakout(instance);
+}
+
+static void flush_callback(libcouchbase_t instance,
+                           const void *cookie,
+                           const char *server_endpoint,
+                           libcouchbase_error_t error)
+{
+    struct user_cookie *c = (void*)instance->cookie;
+
+    restore_user_env(instance);
+    c->callbacks.flush(instance, cookie, server_endpoint, error);
+    restore_wrapping_env(instance, c, error);
+    libcouchbase_maybe_breakout(instance);
+}
+
+
+static void restore_user_env(libcouchbase_t instance)
+{
+    struct user_cookie *cookie = (void*)instance->cookie;
+    /* Restore the users environment */
+    instance->cookie = cookie->cookie;
+    instance->callbacks = cookie->callbacks;
+}
+
+static void restore_wrapping_env(libcouchbase_t instance,
+                                 struct user_cookie *user,
+                                 libcouchbase_error_t error)
+{
+    user->callbacks = instance->callbacks;
+    // Install new callbacks */
+    instance->callbacks.get = get_callback;
+    instance->callbacks.storage = storage_callback;
+    instance->callbacks.arithmetic = arithmetic_callback;
+    instance->callbacks.remove = remove_callback;
+    instance->callbacks.stat = stat_callback;
+    instance->callbacks.touch = touch_callback;
+    instance->callbacks.flush = flush_callback;
+    instance->callbacks.tap_mutation = tap_mutation_callback;
+    instance->callbacks.tap_deletion = tap_deletion_callback;
+    instance->callbacks.tap_flush = tap_flush_callback;
+    instance->callbacks.tap_opaque = tap_opaque_callback;
+    instance->callbacks.tap_vbucket_set = tap_vbucket_set_callback;
+    instance->callbacks.error = error_callback;
+    instance->callbacks.doc_complete = doc_complete_callback;
+    instance->callbacks.doc_data = doc_data_callback;
+
+    user->cookie = (void*)instance->cookie;
+    user->retcode = error;
+    instance->cookie = user;
+}
+
+
+libcouchbase_error_t libcouchbase_synchandler_return(libcouchbase_t instance,
+                                                     libcouchbase_error_t retcode)
 {
     struct user_cookie cookie;
 
@@ -59,18 +310,8 @@ libcouchbase_error_t libcouchbase_synchandler_return(libcouchbase_t instance, li
         return retcode;
     }
 
-    cookie.retcode = LIBCOUCHBASE_SUCCESS;
-
-    /* Save the users environment */
-    cookie.error = instance->callbacks.error;
-    cookie.cookie = (void*)instance->cookie;
-    instance->cookie = &cookie;
-    instance->callbacks.error = error_callback;
-
+    restore_wrapping_env(instance, &cookie, LIBCOUCHBASE_SUCCESS);
     libcouchbase_wait(instance);
-    /* Restore the environment */
-    instance->callbacks.error = cookie.error;
-    instance->cookie = cookie.cookie;
-
+    restore_user_env(instance);
     return cookie.retcode;
 }
---
+++ tests/syncmode-test.c
@@ -0,0 +1,129 @@
+/* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2012 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+#include <string.h>
+#include <assert.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+
+#include "libcouchbase/couchbase.h"
+#include "tests/server.h"
+#include "tests/test.h"
+
+libcouchbase_t session = NULL;
+const void *mock = NULL;
+
+static void error_callback(libcouchbase_t instance,
+                           libcouchbase_error_t err,
+                           const char *errinfo)
+{
+    err_exit("Error %s: %s", libcouchbase_strerror(instance, err), errinfo);
+}
+
+static void store_callback(libcouchbase_t instance,
+                           const void *cookie,
+                           libcouchbase_storage_t operation,
+                           libcouchbase_error_t error,
+                           const void *key, libcouchbase_size_t nkey,
+                           libcouchbase_cas_t cas)
+{
+    int *status = (int*)cookie;
+    *status = error;
+    (void)instance;
+    (void)operation;
+    (void)key;
+    (void)nkey;
+    (void)cas;
+}
+
+static void setup(char **argv)
+{
+    const char *endpoint;
+
+    assert(session == NULL);
+    assert(mock == NULL);
+
+    mock = start_mock_server(argv);
+    if (mock == NULL) {
+        err_exit("Failed to start mock server");
+    }
+
+    endpoint = get_mock_http_server(mock);
+    session = libcouchbase_create(endpoint, "Administrator", "password",
+                                  NULL, NULL);
+    if (session == NULL) {
+        err_exit("Failed to create libcouchbase session");
+    }
+
+    if (libcouchbase_behavior_get_syncmode(session) != LIBCOUCHBASE_ASYNCHRONOUS) {
+        err_exit("The defaul sync mode should be async");
+    }
+
+    (void)libcouchbase_set_error_callback(session, error_callback);
+    if (libcouchbase_connect(session) != LIBCOUCHBASE_SUCCESS) {
+        err_exit("Failed to connect to server");
+    }
+    libcouchbase_wait(session);
+
+    libcouchbase_behavior_set_syncmode(session, LIBCOUCHBASE_SYNCHRONOUS);
+    if (libcouchbase_behavior_get_syncmode(session) != LIBCOUCHBASE_SYNCHRONOUS) {
+        err_exit("set/get of syncmode doesn't work!");
+    }
+
+    libcouchbase_set_storage_callback(session, store_callback);
+}
+
+static void teardown(void)
+{
+    libcouchbase_destroy(session);
+    session = NULL;
+    shutdown_mock_server(mock);
+    mock = NULL;
+}
+
+int main(void)
+{
+    libcouchbase_error_t ret;
+    int error = 0xffff;
+    const char *args[] = {"--nodes", "5", "--buckets=default::memcache", NULL};
+
+    if (getenv("LIBCOUCHBASE_VERBOSE_TESTS") == NULL) {
+        freopen("/dev/null", "w", stdout);
+    }
+
+    setup((char **)args);
+
+    ret = libcouchbase_store(session, &error, LIBCOUCHBASE_SET,
+                             "key", 3, NULL, 0, 0, 0, 0);
+    if (ret != LIBCOUCHBASE_SUCCESS) {
+        err_exit("failed to store key");
+    }
+
+    if (error == 0xffff) {
+        err_exit("libcouchbase_store didn't wait for the callback");
+    }
+    error = 0xffff;
+
+    ret = libcouchbase_store(session, &error, LIBCOUCHBASE_ADD,
+                             "key", 3, NULL, 0, 0, 0, 0);
+    if (ret != LIBCOUCHBASE_KEY_EEXISTS || error != LIBCOUCHBASE_KEY_EEXISTS) {
+        err_exit("Expected the callback to set key eexists");
+    }
+
+    teardown();
+    return 0;
+}
