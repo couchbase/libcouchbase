02114e8deef2aa31a3feb0fe74f0d7f303652410
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-04-07 01:17:38 +0300

    Don't define size_t and ssize_t for VS2008
    https://groups.google.com/d/topic/couchbase/d_vdVruDfGE/discussion
    
    Change-Id: Ie8a46f14063c42803fab538403de409ade004407
    Reviewed-on: http://review.couchbase.org/14642
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Matt Ingenthron <matt@couchbase.com>

--- contrib/http_parser/http_parser.h
+++ contrib/http_parser/http_parser.h
@@ -37,9 +37,6 @@ typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
 typedef __int64 int64_t;
 typedef unsigned __int64 uint64_t;
-
-typedef unsigned int size_t;
-typedef int ssize_t;
 #else
 #include <stdint.h>
 #endif
