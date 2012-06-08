eb97e93473ce61838f76d8fb7bd165d4ba19b6f3
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-06-09 00:24:59 +0300

    Define size_t types to fix MSVC 9 build
    Change-Id: I9cd7316e519261bae8cd9ee561f7cb880ab36867
    Reviewed-on: http://review.couchbase.org/17008
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- contrib/http_parser/http_parser.h
+++ contrib/http_parser/http_parser.h
@@ -37,6 +37,8 @@ typedef __int32 int32_t;
 typedef unsigned __int32 uint32_t;
 typedef __int64 int64_t;
 typedef unsigned __int64 uint64_t;
+typedef unsigned long size_t;
+typedef long ssize_t;
 #else
 #include <stdint.h>
 #endif
