a18aab9c6e358f5ad71babfc0b09599a04afab83
Sergey Avseyev <sergey.avseyev@gmail.com>
2012-09-10 23:25:54 +0300

    Include config.h in unit_tests.cc
    Change-Id: I70c88cfa46c7203d51d4566e5c3c55cecfff9915
    Reviewed-on: http://review.couchbase.org/20706
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Mordechai Nunberg <mnunberg@haskalah.org>
    Tested-by: Mordechai Nunberg <mnunberg@haskalah.org>

--- tests/unit_tests.cc
+++ tests/unit_tests.cc
@@ -14,6 +14,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
+#include "config.h"
 #include <gtest/gtest.h>
 #ifdef HAVE_COUCHBASEMOCK
 #include "mock-environment.h"
