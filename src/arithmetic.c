cf4ac48e30b1310681e0d97104c1b40fc670a1b4
Trond Norbye <trond.norbye@gmail.com>
2012-12-16 18:09:14 +0100

    Fix compiler warning
    Change-Id: I7bbec9c5b48ab4a8226de568485f7cd0133c002c
    Reviewed-on: http://review.couchbase.org/23342
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -94,7 +94,8 @@ lcb_error_t lcb_arithmetic(lcb_t instance,
                    sizeof(req.message.body.expiration));
         }
 
-        TRACE_ARITHMETIC_BEGIN(&req, key, nkey, delta, initial, create ? exp : 0xffffffff);
+        TRACE_ARITHMETIC_BEGIN(&req, key, nkey, delta, initial,
+                               create ? exp : (lcb_time_t)0xffffffff);
         lcb_server_start_packet(server, command_cookie, req.bytes,
                                 sizeof(req.bytes));
         lcb_server_write_packet(server, key, nkey);
