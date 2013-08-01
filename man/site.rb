ef4b6e7a21a677c3ce9a496d6cff10c65ebc348e
Sergey Avseyev <sergey.avseyev@gmail.com>
2013-08-01 19:57:00 +0300

    Append Couchbase CSS to HTML documentation
    Change-Id: Id6f8c156bc7cca43cbf969cf492cd4dd547cfeaf
    Reviewed-on: http://review.couchbase.org/27861
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- man/site.rb
+++ man/site.rb
@@ -50,6 +50,28 @@ sources.each do |file|
 end
 
 FileUtils.cp("#{destdir}/libcouchbase.3lib.html", "#{destdir}/index.html")
+File.open("#{destdir}/docbook-xsl.css", "a+") do |f|
+  f.puts(<<EOS)
+/* couchbase styles */
+body{color:#5b5352;background:#fcfdfb;}
+h1,h2,h3{color:#a30a0a;font-weight:bold;}
+h4,h5,h6{font-weight:normal;}
+h1{font-size:2.5em;line-height:1;margin-bottom:0.5em;}
+h2{font-size:2em;margin-bottom:0.75em;}
+h3{font-size:1.5em;line-height:1;margin-bottom:1em;}
+h4{font-size:1.2em;line-height:1.25;margin-bottom:1.25em;}
+h5{font-size:1em;font-weight:bold;margin-bottom:1.5em;}
+h6{font-size:1em;font-weight:bold;}
+h1 img,h2 img,h3 img,h4 img,h5 img,h6 img{margin:0;}
+a,a:link,a:visited{color:#0a72a3;text-decoration:none;}
+a:hover,a:active,a:focus{color:#a30a0a;text-decoration:underline;outline:none;}
+hr{height:1px;background:#edede5;color:#edede5;border:none;}
+dt span.term{color:#a30a0a;}
+table,tbody,thead,table td, table tr{border:1px solid #c4c1be !important;}
+body pre{color:#5c0000;font-size:90%;}
+EOS
+end
+
 system("zip -9 -r #{File.basename(destdir)}.zip #{destdir}")
 FileUtils.rm_rf(tempdir)
 FileUtils.rm_rf(destdir)
