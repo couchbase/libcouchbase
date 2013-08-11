b85015a155af1d820edce52b4c3f5824c4547562
Mark Nunberg <mnunberg@haskalah.org>
2013-08-10 17:03:56 -0700

    Add callbacks and more checks to CCBC-150 test
    Change-Id: I125674c6a004efafb63c9934615c6380043ba00d
    Reviewed-on: http://review.couchbase.org/28118
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>

--- tests/regression-unit-test.cc
+++ tests/regression-unit-test.cc
@@ -31,11 +31,42 @@ protected:
     }
 };
 
+static bool callbackInvoked = false;
+
+extern "C" {
+static void get_callback(lcb_t, const void *cookie,
+                         lcb_error_t err, const lcb_get_resp_t *)
+{
+    EXPECT_EQ(err, LCB_KEY_ENOENT);
+    int *counter_p = reinterpret_cast<int*>(const_cast<void*>(cookie));
+    EXPECT_TRUE(counter_p != NULL);
+    EXPECT_GT(*counter_p, 0);
+    *counter_p -= 1;
+    callbackInvoked = true;
+}
+
+static void stats_callback(lcb_t, const void *cookie, lcb_error_t err,
+                           const lcb_server_stat_resp_t *resp)
+{
+    EXPECT_EQ(err, LCB_SUCCESS);
+    if (resp->v.v0.nkey == 0) {
+        int *counter_p = reinterpret_cast<int*>(const_cast<void*>(cookie));
+        *counter_p -= 1;
+    }
+    callbackInvoked = true;
+}
+
+}
+
 TEST_F(RegressionUnitTest, CCBC_150)
 {
     lcb_t instance;
     HandleWrap hw;
     createConnection(hw, instance);
+
+    callbackInvoked = false;
+    lcb_set_get_callback(instance, get_callback);
+    lcb_set_stat_callback(instance, stats_callback);
 
     lcb_get_cmd_t getCmd1("testGetMiss1");
     lcb_get_cmd_t *getCmds[] = { &getCmd1 };
@@ -46,15 +77,28 @@ TEST_F(RegressionUnitTest, CCBC_150)
 
     // Lets spool up a lot of commands in one of the buffers so that we
     // know we need to search for it a few times when we get responses..
+    int callbackCounter = 1000;
+    void *ptr = &callbackCounter;
+
     for (ii = 0; ii < 1000; ++ii) {
-        EXPECT_EQ(LCB_SUCCESS, lcb_get(instance, NULL, 1, getCmds));
+        EXPECT_EQ(LCB_SUCCESS, lcb_get(instance, ptr, 1, getCmds));
     }
 
-    EXPECT_EQ(LCB_SUCCESS, lcb_server_stats(instance, NULL, 1, statCmds));
+    callbackCounter++;
+    EXPECT_EQ(LCB_SUCCESS, lcb_server_stats(instance, ptr, 1, statCmds));
+
+    callbackCounter += 1000;
     for (ii = 0; ii < 1000; ++ii) {
-        EXPECT_EQ(LCB_SUCCESS, lcb_get(instance, NULL, 1, getCmds));
+        EXPECT_EQ(LCB_SUCCESS, lcb_get(instance, ptr, 1, getCmds));
     }
-    EXPECT_EQ(LCB_SUCCESS, lcb_server_stats(instance, NULL, 1, statCmds));
-    EXPECT_EQ(LCB_SUCCESS, lcb_server_stats(instance, NULL, 1, statCmds));
-    lcb_wait(instance);
+
+    callbackCounter++;
+    EXPECT_EQ(LCB_SUCCESS, lcb_server_stats(instance, ptr, 1, statCmds));
+
+    callbackCounter++;
+    EXPECT_EQ(LCB_SUCCESS, lcb_server_stats(instance, ptr, 1, statCmds));
+
+    EXPECT_EQ(LCB_SUCCESS, lcb_wait(instance));
+    ASSERT_TRUE(callbackInvoked);
+    ASSERT_EQ(callbackCounter, 0);
 }
