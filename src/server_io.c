a78e86b21d7131a3a9a9d985904e0f45ec0d94a3
Mark Nunberg <mnunberg@haskalah.org>
2013-09-16 18:09:30 -0700

    Reset 'weird_things' upon success
    Change-Id: I11f3aefda677ee4ca7ba841f662c68f9b7c9c73a
    Reviewed-on: http://review.couchbase.org/29018
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- src/server_io.c
+++ src/server_io.c
@@ -73,6 +73,9 @@ static void event_complete_common(lcb_server_t *c, lcb_error_t rc)
     if (rc != LCB_SUCCESS) {
         lcb_failout_server(c, rc);
     } else {
+        if (c->is_config_node) {
+            c->instance->weird_things = 0;
+        }
         lcb_sockrw_apply_want(&c->connection);
         c->inside_handler = 0;
     }
