61cb320874ffaec95b886b4a5d68889b9413ddf9
Mark Nunberg <mnunberg@haskalah.org>
2014-01-16 05:24:35 -0800

    Decouple lcb_N_t from versioning info
    Change-Id: Ie63ac7f13b31ebefbff7efb47aed8075f2aa8904
    Reviewed-on: http://review.couchbase.org/32558
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>
    Reviewed-by: Brett Lawson <brett19@gmail.com>

--- win32/libcouchbase/configuration.h
+++ win32/libcouchbase/configuration.h
@@ -28,34 +28,8 @@
 #error "Include libcouchbase/couchbase.h instead"
 #endif
 
-#include <stddef.h>
-
 #define LCB_VERSION_STRING "2.2.0"
 #define LCB_VERSION 0x020200
 #define LCB_VERSION_CHANGESET "unknown"
 #define PACKAGE_STRING "libcouchbase 2.2.0"
-
-#include <winsock2.h>
-#include <basetsd.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-typedef SIZE_T lcb_size_t;
-typedef SSIZE_T lcb_ssize_t;
-typedef __int64 lcb_int64_t;
-typedef __int32 lcb_int32_t;
-typedef unsigned __int8 lcb_uint8_t;
-typedef unsigned __int16 lcb_vbucket_t;
-typedef unsigned __int16 lcb_uint16_t;
-typedef unsigned __int32 lcb_uint32_t;
-typedef unsigned __int64 lcb_cas_t;
-typedef unsigned __int64 lcb_uint64_t;
-typedef unsigned __int32 lcb_time_t;
-
-#ifdef __cplusplus
-}
-#endif
-
 #endif
