596965b32ef613929580484373090d325f64b9a1
Mark Nunberg <mnunberg@haskalah.org>
2014-01-22 09:22:06 -0800

    HTTP: Use "Easy" I/O
    Change-Id: I3ceab99c05552cd1081f9f2648fa031a463e149b
    Reviewed-on: http://review.couchbase.org/32739
    Reviewed-by: Brett Lawson <brett19@gmail.com>
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>

--- src/http_io.c
+++ src/http_io.c
@@ -16,176 +16,6 @@
  */
 #include "internal.h"
 
-static void request_v0_handler(lcb_socket_t, short, void *);
-
-static void request_v1_write_handler(lcb_sockdata_t *sock,
-                                     lcb_io_writebuf_t *wbuf,
-                                     int status);
-
-static void request_v1_read_handler(lcb_sockdata_t *sock, lcb_ssize_t nr);
-static void request_v1_error_handler(lcb_sockdata_t *sock);
-static int request_do_parse(lcb_http_request_t req);
-
-static void request_v0_handler(lcb_socket_t sock, short which, void *arg)
-{
-    lcb_connection_t conn = arg;
-    lcb_http_request_t req = conn->data;
-    lcb_t instance = req->instance;
-    lcb_ssize_t rv;
-    int should_continue = 1;
-    lcb_error_t err = LCB_SUCCESS;
-
-    req->refcount++;
-
-    if (which & LCB_READ_EVENT) {
-        lcb_sockrw_status_t status;
-
-        status = lcb_sockrw_v0_slurp(&req->connection, req->connection.input);
-        if (status != LCB_SOCKRW_READ &&
-                status != LCB_SOCKRW_WOULDBLOCK &&
-                status != LCB_SOCKRW_SHUTDOWN) {
-            should_continue = 0;
-            err = LCB_NETWORK_ERROR;
-
-        } else {
-            rv = request_do_parse(req);
-            if (rv == 0) {
-                /* Request completed */
-                should_continue = 0;
-
-            } else if (rv < 0) {
-                /* Request format error */
-                should_continue = 0;
-                if (req->redirect_to) {
-                    lcb_settings *settings = &instance->settings;
-                    if (settings->max_redir != -1 && settings->max_redir ==  req->redircount) {
-                        err = LCB_TOO_MANY_REDIRECTS;
-                        req->redirect_to = NULL;
-                    }
-                } else {
-                    err = LCB_PROTOCOL_ERROR;
-                }
-            } else {
-                /* Still want more data: */
-                lcb_sockrw_set_want(&req->connection, LCB_READ_EVENT, 1);
-            }
-
-            if (status == LCB_SOCKRW_SHUTDOWN && should_continue != 0) {
-                /** Premature termination of connection */
-                err = LCB_NETWORK_ERROR;
-                should_continue = 0;
-            }
-        }
-    }
-
-    if (should_continue && (which & LCB_WRITE_EVENT)) {
-        lcb_sockrw_status_t status;
-
-        status = lcb_sockrw_v0_write(&req->connection, req->connection.output);
-
-        if (status != LCB_SOCKRW_WROTE && status != LCB_SOCKRW_WOULDBLOCK) {
-            err = LCB_NETWORK_ERROR;
-            should_continue = 0;
-
-        } else if (lcb_sockrw_flushed(&req->connection)) {
-            lcb_sockrw_set_want(&req->connection, LCB_READ_EVENT, 1);
-
-        } else {
-            lcb_sockrw_set_want(&req->connection, LCB_RW_EVENT, 1);
-        }
-    }
-
-    if (!should_continue) {
-        if (req->redirect_to) {
-            req->url = req->redirect_to;
-            req->nurl = strlen(req->url);
-            req->redirect_to = NULL;
-            err = lcb_http_verify_url(req, NULL, 0);
-            if (err == LCB_SUCCESS) {
-                err = lcb_http_request_exec(req);
-            }
-            if (err != LCB_SUCCESS) {
-                lcb_http_request_finish(instance, req, err);
-            }
-        } else {
-            lcb_http_request_finish(instance, req, err);
-        }
-    } else {
-        lcb_sockrw_apply_want(&req->connection);
-    }
-
-    lcb_http_request_decref(req);
-    (void)sock;
-}
-
-
-static void request_v1_error_handler(lcb_sockdata_t *sock)
-{
-    lcb_http_request_t req;
-    if (!lcb_sockrw_v1_cb_common(sock, NULL, (void **)&req)) {
-        return;
-    }
-    lcb_http_request_finish(req->instance, req, LCB_NETWORK_ERROR);
-}
-
-static void request_v1_read_handler(lcb_sockdata_t *sock, lcb_ssize_t nr)
-{
-    int rv;
-    lcb_http_request_t req;
-    if (!lcb_sockrw_v1_cb_common(sock, NULL, (void **)&req)) {
-        return;
-    }
-
-    lcb_sockrw_v1_onread_common(sock, &req->connection.input, nr);
-
-    if (nr < 1) {
-        lcb_http_request_finish(req->instance, req, LCB_NETWORK_ERROR);
-        return;
-    }
-
-    req->refcount++;
-    rv = request_do_parse(req);
-
-    if (rv < 1) {
-        lcb_http_request_finish(req->instance,
-                                req, rv ? LCB_PROTOCOL_ERROR : LCB_SUCCESS);
-
-    } else {
-        lcb_sockrw_set_want(&req->connection, LCB_READ_EVENT, 1);
-        lcb_sockrw_apply_want(&req->connection);
-    }
-
-    lcb_http_request_decref(req);
-}
-
-static void request_v1_write_handler(lcb_sockdata_t *sock,
-                                     lcb_io_writebuf_t *wbuf,
-                                     int status)
-{
-    lcb_http_request_t req;
-
-    if (!lcb_sockrw_v1_cb_common(sock, wbuf, (void **)&req)) {
-        return;
-    }
-
-    lcb_sockrw_v1_onwrite_common(sock, wbuf, &req->connection.input);
-
-    if (status) {
-        lcb_http_request_finish(req->instance, req, LCB_NETWORK_ERROR);
-
-    } else {
-        lcb_sockrw_set_want(&req->connection, LCB_READ_EVENT, 1);
-        lcb_sockrw_apply_want(&req->connection);
-    }
-}
-
-
-static void request_timed_out(lcb_connection_t conn, lcb_error_t err)
-{
-    lcb_http_request_t req = (lcb_http_request_t)conn->data;
-    lcb_http_request_finish(req->instance, req, err);
-}
-
 static int request_do_parse(lcb_http_request_t req)
 {
     int rv = lcb_http_request_do_parse(req);
@@ -196,6 +26,71 @@ static int request_do_parse(lcb_http_request_t req)
     return rv;
 }
 
+static void io_read(lcb_connection_t conn)
+{
+    lcb_http_request_t req = conn->data;
+    lcb_t instance = req->instance;
+    int rv, is_done = 0;
+    lcb_error_t err = LCB_SUCCESS;
+
+    /** Delay the timer */
+    lcb_connection_delay_timer(conn);
+
+    rv = request_do_parse(req);
+    if (rv == 0) {
+        is_done = 1;
+
+    } else if (rv < 0) {
+        is_done = 1;
+
+        if (req->redirect_to) {
+            lcb_settings *settings = &instance->settings;
+            if (settings->max_redir != -1 &&
+                      settings->max_redir == req->redircount) {
+                err = LCB_TOO_MANY_REDIRECTS;
+                req->redirect_to = NULL;
+            }
+        } else {
+            err = LCB_PROTOCOL_ERROR;
+        }
+    } else {
+        lcb_sockrw_set_want(conn, LCB_READ_EVENT, 1);
+    }
+
+    if (is_done) {
+        if (req->redirect_to) {
+            req->url = req->redirect_to;
+            req->nurl = strlen(req->url);
+            req->redirect_to = NULL;
+            err = lcb_http_verify_url(req, NULL, 0);
+            if (err == LCB_SUCCESS) {
+                err = lcb_http_request_exec(req);
+            } else {
+                lcb_http_request_finish(instance, req, err);
+            }
+        } else {
+            lcb_http_request_finish(instance, req, err);
+        }
+    } else {
+        lcb_sockrw_apply_want(conn);
+    }
+
+    lcb_http_request_decref(req);
+}
+
+static void io_error(lcb_connection_t conn)
+{
+    lcb_http_request_t req = conn->data;
+    lcb_http_request_finish(req->instance, req, LCB_NETWORK_ERROR);
+}
+
+static void request_timed_out(lcb_connection_t conn, lcb_error_t err)
+{
+    lcb_http_request_t req = (lcb_http_request_t)conn->data;
+    lcb_http_request_finish(req->instance, req, err);
+}
+
+
 
 static void request_connected(lcb_connection_t conn, lcb_error_t err)
 {
@@ -205,35 +100,31 @@ static void request_connected(lcb_connection_t conn, lcb_error_t err)
         return;
     }
 
-    req->connection.evinfo.handler = request_v0_handler;
-    req->connection.completion.read = request_v1_read_handler;
-    req->connection.completion.write = request_v1_write_handler;
-    req->connection.completion.error = request_v1_error_handler;
     lcb_sockrw_set_want(&req->connection, LCB_WRITE_EVENT, 1);
     lcb_sockrw_apply_want(&req->connection);
 }
 
 lcb_error_t lcb_http_request_connect(lcb_http_request_t req)
 {
+    struct lcb_io_use_st use;
     lcb_connection_result_t result;
+    lcb_uint32_t timeout;
     lcb_connection_t conn = &req->connection;
     conn->on_connect_complete = request_connected;
     conn->on_timeout = request_timed_out;
 
-    if (req->reqtype == LCB_HTTP_TYPE_VIEW) {
-        conn->timeout.usec = req->instance->settings.views_timeout;
+    timeout = req->reqtype == LCB_HTTP_TYPE_VIEW ?
+            req->instance->settings.views_timeout :
+            req->instance->settings.http_timeout;
 
-    } else {
-        conn->timeout.usec = req->instance->settings.http_timeout;
-    }
-
-    conn->data = req;
     lcb_connection_getaddrinfo(conn, 0);
-    result = lcb_connection_start(conn, 1);
 
+    result = lcb_connection_start(conn, 1);
     if (result != LCB_CONN_INPROGRESS) {
         return LCB_CONNECT_ERROR;
     }
 
+    lcb_connuse_easy(&use, req, timeout, io_read, io_error, request_timed_out);
+    lcb_connection_use(conn, &use);
     return LCB_SUCCESS;
 }
