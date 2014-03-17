9cbb2381e19df327715cbcc07cf5f7d7a07f241f
Mark Nunberg <mnunberg@haskalah.org>
2014-03-17 09:22:13 -0700

    Add header aliasing vbucket functions to shorter names
    Change-Id: I874c0fc4c09535aab5eb67dd51a9bd4fbbd0bd6c
    Reviewed-on: http://review.couchbase.org/34597
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>
    Reviewed-by: Brett Lawson <brett19@gmail.com>

---
+++ src/vb-aliases.h
@@ -0,0 +1,12 @@
+#ifndef VB_ALIASES_H
+#define VB_ALIASES_H
+
+/** Aliases for libvbucket */
+#define VB_REMAP vbucket_found_incorrect_master
+#define VB_NODESTR vbucket_config_get_server
+#define VB_VIEWSURL vbucket_config_get_couch_api_base
+#define VB_RESTURL vbucket_config_get_rest_api_server
+#define VB_DISTTYPE vbucket_config_get_distribution_type
+#define VB_NREPLICAS vbucket_config_get_num_replicas
+#define VB_NSERVERS vbucket_config_get_num_servers
+#endif
