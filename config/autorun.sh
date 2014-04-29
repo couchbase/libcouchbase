02a6ac9d438377cc0d063238b5d42d342bbcc474
Mark Nunberg <mnunberg@haskalah.org>
2014-04-29 15:17:05 -0700

    autorun: place perl script _before_ generation
    Otherwise we try to generate the configure/Makefile.in stuff and it
    complains about filelist.mk not existing
    
    Change-Id: I3742e5ffb6700474816b23e174862ef1b6a2582e
    Reviewed-on: http://review.couchbase.org/36526
    Reviewed-by: Mark Nunberg <mnunberg@haskalah.org>
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>

--- config/autorun.sh
+++ config/autorun.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 set -e
-
+./config/gensrclist.pl
 cat > m4/version.m4 <<EOF
 m4_define([VERSION_NUMBER], [`git describe | tr '-' '_'`])
 m4_define([GIT_CHANGESET],[`git rev-parse HEAD`])
 EOF
 
 autoreconf -i --force
-./config/gensrclist.pl
