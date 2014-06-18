6664b9e790b43135dd423ae1093d728a413c56c5
Mark Nunberg <mnunberg@haskalah.org>
2014-06-18 13:38:48 -0700

    Update packaging scripts
    This consists of a set of changes allowing proper packaging for the
    newer library version. These changes are:
    
    - Use common scripts to deduce version information
    - Remove man3 and cbcrc manpages from installation list
    - Build with a static snappy on RPM platforms
    - Debian platforms require a system-provided libsnappy
    - Enhance debian script for paralell building
    
    Change-Id: If9f6e973cd85d9bec6a3208e997bad93a04de62d
    Reviewed-on: http://review.couchbase.org/38569
    Reviewed-by: Matt Ingenthron <matt@couchbase.com>
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>

--- config/autorun.sh
+++ config/autorun.sh
@@ -4,6 +4,7 @@ set -e
 ./config/gensrclist.pl
 cat > m4/version.m4 <<EOF
 m4_define([VERSION_NUMBER], [`git describe | tr '-' '_'`])
+m4_define([GIT_DESCRIBE], [`git describe --long`])
 m4_define([GIT_CHANGESET],[`git rev-parse HEAD`])
 EOF
 
