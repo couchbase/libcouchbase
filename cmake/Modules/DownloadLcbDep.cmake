53264f9ed2dd6f24707e4fd8f3cb1491a100e821
Mark Nunberg <mnunberg@haskalah.org>
2014-06-21 11:49:53 -0700

    cmake: Fix download handling
    Sometimes cmake's download handling gets stuck on various platforms.
    This abstracts downloading into its own module and employs either wget
    or powershell to perform the actual download.
    
    Change-Id: I1dd01b2b8559455549d03d088d13e8b509d89550
    Reviewed-on: http://review.couchbase.org/38681
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>
    Reviewed-by: Brett Lawson <brett19@gmail.com>

---
+++ cmake/Modules/DownloadLcbDep.cmake
@@ -0,0 +1,12 @@
+# DownloadLcbDeb(url location)
+MACRO(DOWNLOAD_LCB_DEP url dest)
+    IF(UNIX)
+        EXECUTE_PROCESS(COMMAND wget -O "${dest}" "${url}")
+    ELSEIF(WIN32)
+        EXECUTE_PROCESS(COMMAND powershell -Command
+            "(New-Object Net.WebClient).DownloadFile('${url}', '${dest}')")
+    ELSE()
+        MESSAGE(WARNING "Using buggy built-in CMake downloader")
+        FILE(DOWNLOAD ${url} ${dest} INACTIVITY_TIMEOUT 30 SHOW_PROGRESS)
+    ENDIF()
+ENDMACRO()
