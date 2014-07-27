60de6f340a4203227c3c3bc9b697967aed20a365
Mark Nunberg <mnunberg@haskalah.org>
2014-07-27 10:33:15 -0700

    instancepool: Remove older C prototypes
    Unfortunately we overlooked this in the previous commit
    
    Change-Id: I53a55b1db4536e61a67c496f04d024a6cc738714
    Reviewed-on: http://review.couchbase.org/39954
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>
    Reviewed-by: Brett Lawson <brett19@gmail.com>

--- example/instancepool/pool.h
+++ example/instancepool/pool.h
@@ -65,47 +65,4 @@ private:
 };
 } // namespace
 
-
-/**
- * The clients of a connection pool should not need to know the
- * internals of the pool. All they want to do is to grab an instance
- * for a while
- */
-typedef struct lcb_pool_st* lcb_pool_t;
-
-/**
- * Create a pool of connections that the client may grab to perform some
- * operations and then release it back to the pool.
- *
- * @param size the number of instances in the pool
- * @param options the options to create the pool with (hostname, bucket etc).
- *                please note that you <b>cannot</b> specify an IO object here
- *                because we don't know if it is threadsafe or not...
- * @param pool the created pool is returned here
- * @param initiate a function called for all of the created instances to
- *                 allow you to specify the callbacks and set options etc.
- * @return LCB_SUCCESS on success, or an appropriate error code on failure
- */
-lcb_error_t pool_create(unsigned int size,
-                        const struct lcb_create_st *options,
-                        lcb_pool_t *pool,
-                        void (*initiate)(lcb_t));
-
-/**
- * Get an instance from the connection pool. If none is available
- * the caller is <b>blocked</b> until one is available.
- *
- * @param pool the pool to allocate the instance from
- * @return a libcoucbase instance
- */
-lcb_t pool_pop(lcb_pool_t pool);
-
-/**
- * Put the libcouchbase instance back into the pool.
- *
- * @param pool where to store the instance
- * @param instance the instance to release
- */
-void pool_push(lcb_pool_t pool, lcb_t instance);
-
 #endif
