3f53dee1f4c4bdac539badbd6b22d777c523fe6d
Mark Nunberg <mnunberg@haskalah.org>
2014-07-27 10:24:09 -0700

    Fix up instancepool example
    This patch rewrites the instance pool example in C++ which has built in
    STL types for managing lists and such, and allows the example to focus
    more on the lcb-specific logic needed for instantiating a pool.
    
    This rewrite also demonstrates how one might actually _use_ such a pool
    by spinning up some "Worker" threads to perform operations.
    
    Change-Id: If98c5263315a7c190726e411cf778b142c3a041c
    Reviewed-on: http://review.couchbase.org/39933
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>
    Reviewed-by: Brett Lawson <brett19@gmail.com>

--- example/instancepool/pool.h
+++ example/instancepool/pool.h
@@ -18,11 +18,53 @@
 #ifndef POOL_H
 #define POOL_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <libcouchbase/couchbase.h>
+#include <pthread.h>
+#include <queue>
+#include <vector>
+
+namespace lcb {
+
+class Pool {
+public:
+    /**
+     * Create a new pool to use across threads
+     * @param options The options used to initialize the instance
+     * @param items How many items should be in the pool
+     */
+    Pool(const lcb_create_st& options, size_t items = 10);
+    virtual ~Pool();
+
+    /**Get an instance from the pool. You should call #push() when you are
+     * done with the instance
+     * @return an lcb_t instance */
+    lcb_t pop();
+
+    /**Release an instance back into the pool
+     * @param instance The instance to release */
+    void push(lcb_t instance);
+
+    // Connect all the instances in the pool. This should be called once the
+    // pool has been constructed
+    lcb_error_t connect();
+
+protected:
+    /**Function called after the instance is created. You may
+     * customize the instance here with e.g. lcb_set_cookie()
+     * @param instance the newly created instance */
+    virtual void initialize(lcb_t instance) = 0;
+
+private:
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    std::queue<lcb_t> instances;
+
+    // List of all instances
+    std::vector<lcb_t> all_instances;
+    size_t initial_size;
+};
+} // namespace
+
 
 /**
  * The clients of a connection pool should not need to know the
@@ -65,9 +107,5 @@ lcb_t pool_pop(lcb_pool_t pool);
  * @param instance the instance to release
  */
 void pool_push(lcb_pool_t pool, lcb_t instance);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
