1c3f3d018f2435fe4e85f0393f8f0cd85b936851
Mark Nunberg <mnunberg@haskalah.org>
2014-10-08 16:15:57 -0700

    Confmon: log revision ID
    Change-Id: Ibced5b4ccae007b37960436b63fad33ada197e2d
    Reviewed-on: http://review.couchbase.org/41978
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>

--- src/bucketconfig/confmon.c
+++ src/bucketconfig/confmon.c
@@ -158,7 +158,12 @@ static int do_set_next(lcb_confmon *mon, clconfig_info *info, int notify_miss)
         lcbvb_free_diff(diff);
 
         if (chstatus == 0 || lcb_clconfig_compare(mon->config, info) >= 0) {
-            lcb_log(LOGARGS(mon, INFO), "Not applying configuration received via %s. No changes detected", provider_string(info->origin));
+            const lcbvb_CONFIG *ca, *cb;
+
+            ca = mon->config->vbc;
+            cb = info->vbc;
+
+            lcb_log(LOGARGS(mon, INFO), "Not applying configuration received via %s. No changes detected. A.rev=%d, B.rev=%d", provider_string(info->origin), ca->revid, cb->revid);
             if (notify_miss) {
                 invoke_listeners(mon, CLCONFIG_EVENT_GOT_ANY_CONFIG, info);
             }
