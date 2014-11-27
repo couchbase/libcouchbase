ea63daec74f4675777dc0e165afd4a42636edefb
Mark Nunberg <mnunberg@haskalah.org>
2014-11-27 10:15:40 -0800

    Move all documentation to 'doc' directory
    Remove clutter from top level directory
    
    Change-Id: I458e424148762b0b8131743fce97f326fe6439fe
    Reviewed-on: http://review.couchbase.org/43689
    Tested-by: Mark Nunberg <mnunberg@haskalah.org>
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- doc/genman.sh
+++ doc/genman.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 set -x
-OUTDIR=.
-SRCDIR=../doc
+
+# Run from the top level source directory
+OUTDIR=man
+SRCDIR=.
 
 ronn --pipe --roff $SRCDIR/cbc.markdown > $OUTDIR/cbc.1
 ronn --pipe --roff $SRCDIR/cbc-pillowfight.markdown > $OUTDIR/cbc-pillowfight.1
@@ -11,10 +13,7 @@ ronn --pipe --roff $SRCDIR/cbcrc.markdown > $OUTDIR/cbcrc.4
 MANLINKS="cat cp create observe flush hash lock unlock rm stats"
 MANLINKS="$MANLINKS version verbosity view admin bucket-create bucket-delete connstr"
 
-> manpages.mk
-
 for link in $MANLINKS; do
     dest="$OUTDIR/cbc-${link}.1"
     echo ".so man1/cbc.1" > "$dest"
-    echo "man_MANS+=man/cbc-${link}.1" >> manpages.mk
 done
