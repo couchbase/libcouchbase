900b70c570ddedcc8652d084a58b58cdb64d5f98
Subhashni Balakrishnan <b.subhashni@gmail.com>
2015-03-18 17:04:43 -0700

    Return http error code only when http response is not ok
    Removed overwriting of failure codes always with http error code
    Added a log message for not ok http response status
    
    Change-Id: I2d4eef16db1c7f66ba8a52e456ef32d35ea8e66a
    Reviewed-on: http://review.couchbase.org/48486
    Reviewed-by: Mark Nunberg <mark.nunberg@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>

--- src/views/viewreq.c
+++ src/views/viewreq.c
@@ -17,6 +17,7 @@ static void unref_request(lcbview_REQUEST *req);
 
 /* Whether the request (from the user side) is still ongoing */
 #define CAN_CONTINUE(req) ((req)->callback != NULL)
+#define LOGARGS(instance, lvl) instance->settings, "views", LCB_LOG_##lvl, __FILE__, __LINE__
 
 static void
 invoke_last(lcbview_REQUEST *req, lcb_error_t err)
@@ -65,7 +66,12 @@ chunk_callback(lcb_t instance, int cbtype, const lcb_RESPBASE *rb)
 
     if (rh->rc != LCB_SUCCESS || rh->htstatus != 200 || (rh->rflags & LCB_RESP_F_FINAL)) {
         if (req->lasterr == LCB_SUCCESS && rh->htstatus != 200) {
-            req->lasterr = LCB_HTTP_ERROR;
+            if (rh->rc != LCB_SUCCESS) {
+                req->lasterr = rh->rc;
+            } else {
+                lcb_log(LOGARGS(instance, DEBUG), "Got not ok http status %d", rh->htstatus);
+                req->lasterr = LCB_HTTP_ERROR;
+            }
         }
         req->refcount++;
         invoke_last(req, req->lasterr);
