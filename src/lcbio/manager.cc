f170bbce5d41cfdc0504b9175b2556a21c55acdb
Sergey Avseyev <sergey.avseyev@gmail.com>
2019-10-15 22:06:20 +0300

    CCBC-1106: push down boring log messages from IO manager
    Change-Id: I894f97c96c99864fcda1e20b12170c6b0fb2c583
    Reviewed-on: http://review.couchbase.org/116456
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Ellis Breen <ellis.breen@couchbase.com>

--- src/lcbio/manager.cc
+++ src/lcbio/manager.cc
@@ -468,7 +468,7 @@ GT_POPAGAIN:
         }
 
         req->set_ready(info);
-        lcb_log(LOGARGS(this, INFO),
+        lcb_log(LOGARGS(this, DEBUG),
                 HE_LOGFMT "Found ready connection in pool. Reusing socket and not creating new connection",
                 HE_LOGID(he));
 
@@ -532,7 +532,7 @@ void Pool::put(lcbio_SOCKET *sock)
         return;
     }
 
-    lcb_log(LOGARGS(mgr, INFO), HE_LOGFMT "Placing socket back into the pool. I=%p,C=%p", HE_LOGID(he), (void *)info,
+    lcb_log(LOGARGS(mgr, DEBUG), HE_LOGFMT "Placing socket back into the pool. I=%p,C=%p", HE_LOGID(he), (void *)info,
             (void *)sock);
     info->idle_timer.rearm(mgr->options.tmoidle);
     lcb_clist_append(&he->ll_idle, info);
