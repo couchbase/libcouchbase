71b77007be9d0304aaf61c7da5768175cc6d91f4
Sergey Avseyev <sergey.avseyev@gmail.com>
2019-11-19 10:50:51 +0300

    CCBC-1122: expose setting for N1QL grace period
    The example below will add extra 100ms to each N1QL query:
    
      lcb_cntl_setu32(instance, LCB_CNTL_N1QL_GRACE_PERIOD, 100000);
    
    Change-Id: I2a418867832995c619ffd12f35b82d07d9408649
    Reviewed-on: http://review.couchbase.org/118110
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Sergey Avseyev <sergey.avseyev@gmail.com>

--- src/n1ql/n1ql.cc
+++ src/n1ql/n1ql.cc
@@ -726,7 +726,7 @@ lcb_N1QLREQ::lcb_N1QLREQ(lcb_t obj,
         lasterr = LCB_EINVAL;
         return;
     }
-    timer.rearm(timeout);
+    timer.rearm(timeout + LCBT_SETTING(obj, n1ql_grace_period));
 
     // Determine if we need to add more credentials.
     // Because N1QL multi-bucket auth will not work on server versions < 4.5
