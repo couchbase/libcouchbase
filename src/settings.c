9e06f116326f86e755e02ef0b2523ca04cb922a2
Sergey Avseyev <sergey.avseyev@gmail.com>
2020-01-01 12:02:01 +0300

    Fix compiler warnings for windows
    Change-Id: I023d6fd0e19bcf12ee7be308c3981e6456be305e
    Reviewed-on: http://review.couchbase.org/119878
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Brett Lawson <brett19@gmail.com>

--- src/settings.c
+++ src/settings.c
@@ -47,7 +47,7 @@ void lcb_default_settings(lcb_settings *settings)
     settings->bc_http_urltype = LCB_DEFAULT_HTCONFIG_URLTYPE;
     settings->compressopts = LCB_DEFAULT_COMPRESSOPTS;
     settings->compress_min_size = LCB_DEFAULT_COMPRESS_MIN_SIZE;
-    settings->compress_min_ratio = LCB_DEFAULT_COMPRESS_MIN_RATIO;
+    settings->compress_min_ratio = (float)LCB_DEFAULT_COMPRESS_MIN_RATIO;
     settings->allocator_factory = rdb_bigalloc_new;
     settings->detailed_neterr = 0;
     settings->refresh_on_hterr = 1;
