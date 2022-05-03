164c3936f3633ba028030fd68eebf29dc9cdf029
Sergey Avseyev <sergey.avseyev@gmail.com>
2022-05-03 14:54:31 +0300

    CCBC-1540: bundle capella ca certificate with SDK
    Change-Id: Icbce640e662e7bbb7af497897e1e9232369d897f
    Reviewed-on: https://review.couchbase.org/c/libcouchbase/+/174420
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: David Kelly <davidmichaelkelly@gmail.com>

--- src/ssl/ssl_common.c
+++ src/ssl/ssl_common.c
@@ -32,6 +32,26 @@
 #define LOGARGS(ssl, lvl) ((lcbio_SOCKET *)SSL_get_app_data(ssl))->settings, "SSL", lvl, __FILE__, __LINE__
 static char *global_event = "dummy event for ssl";
 
+static const char *capella_ca_cert = "-----BEGIN CERTIFICATE-----\n"
+                                     "MIIDFTCCAf2gAwIBAgIRANLVkgOvtaXiQJi0V6qeNtswDQYJKoZIhvcNAQELBQAw\n"
+                                     "JDESMBAGA1UECgwJQ291Y2hiYXNlMQ4wDAYDVQQLDAVDbG91ZDAeFw0xOTEyMDYy\n"
+                                     "MjEyNTlaFw0yOTEyMDYyMzEyNTlaMCQxEjAQBgNVBAoMCUNvdWNoYmFzZTEOMAwG\n"
+                                     "A1UECwwFQ2xvdWQwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCfvOIi\n"
+                                     "enG4Dp+hJu9asdxEMRmH70hDyMXv5ZjBhbo39a42QwR59y/rC/sahLLQuNwqif85\n"
+                                     "Fod1DkqgO6Ng3vecSAwyYVkj5NKdycQu5tzsZkghlpSDAyI0xlIPSQjoORA/pCOU\n"
+                                     "WOpymA9dOjC1bo6rDyw0yWP2nFAI/KA4Z806XeqLREuB7292UnSsgFs4/5lqeil6\n"
+                                     "rL3ooAw/i0uxr/TQSaxi1l8t4iMt4/gU+W52+8Yol0JbXBTFX6itg62ppb/Eugmn\n"
+                                     "mQRMgL67ccZs7cJ9/A0wlXencX2ohZQOR3mtknfol3FH4+glQFn27Q4xBCzVkY9j\n"
+                                     "KQ20T1LgmGSngBInAgMBAAGjQjBAMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYE\n"
+                                     "FJQOBPvrkU2In1Sjoxt97Xy8+cKNMA4GA1UdDwEB/wQEAwIBhjANBgkqhkiG9w0B\n"
+                                     "AQsFAAOCAQEARgM6XwcXPLSpFdSf0w8PtpNGehmdWijPM3wHb7WZiS47iNen3oq8\n"
+                                     "m2mm6V3Z57wbboPpfI+VEzbhiDcFfVnK1CXMC0tkF3fnOG1BDDvwt4jU95vBiNjY\n"
+                                     "xdzlTP/Z+qr0cnVbGBSZ+fbXstSiRaaAVcqQyv3BRvBadKBkCyPwo+7svQnScQ5P\n"
+                                     "Js7HEHKVms5tZTgKIw1fbmgR2XHleah1AcANB+MAPBCcTgqurqr5G7W2aPSBLLGA\n"
+                                     "fRIiVzm7VFLc7kWbp7ENH39HVG6TZzKnfl9zJYeiklo5vQQhGSMhzBsO70z4RRzi\n"
+                                     "DPFAN/4qZAgD5q3AFNIq2WWADFQGSwVJhg==\n"
+                                     "-----END CERTIFICATE-----\n";
+
 /******************************************************************************
  ******************************************************************************
  ** Boilerplate lcbio_TABLE Wrappers                                         **
@@ -298,6 +318,48 @@ static long decode_ssl_protocol(const char *protocol)
     return disallow;
 }
 
+static lcb_STATUS add_certificate_authority(const lcb_settings *settings, SSL_CTX *ctx, const char *certificate_value,
+                                            int certificate_length)
+{
+    lcb_STATUS rc = LCB_SUCCESS;
+    ERR_clear_error();
+
+    BIO *bio = BIO_new_mem_buf(certificate_value, certificate_length);
+    if (bio) {
+        X509_STORE *store = SSL_CTX_get_cert_store(ctx);
+        if (store) {
+            for (int added = 0;; added = 1) {
+                X509 *cert = PEM_read_bio_X509(bio, 0, 0, 0);
+                if (!cert) {
+                    unsigned long err = ERR_get_error();
+                    if (added && ERR_GET_LIB(err) == ERR_LIB_PEM && ERR_GET_REASON(err) == PEM_R_NO_START_LINE) {
+                        break;
+                    }
+                    lcb_log(LOGARGS_S(settings, LCB_LOG_ERROR),
+                            "Unable to load default certificate: lib=%s, func=%s, reason=%s", ERR_lib_error_string(err),
+                            ERR_func_error_string(err), ERR_reason_error_string(err));
+                    rc = LCB_ERR_SSL_ERROR;
+                    goto GT_CLEANUP;
+                }
+
+                int ok = X509_STORE_add_cert(store, cert);
+                X509_free(cert);
+                if (ok != 1) {
+                    unsigned long err = ERR_get_error();
+                    lcb_log(LOGARGS_S(settings, LCB_LOG_ERROR),
+                            "Unable to add default certificate: lib=%s, func=%s, reason=%s", ERR_lib_error_string(err),
+                            ERR_func_error_string(err), ERR_reason_error_string(err));
+                    rc = LCB_ERR_SSL_ERROR;
+                    goto GT_CLEANUP;
+                }
+            }
+        }
+    }
+GT_CLEANUP:
+    BIO_free(bio);
+    return rc;
+}
+
 lcbio_pSSLCTX lcbio_ssl_new(const char *tsfile, const char *cafile, const char *keyfile, int noverify, lcb_STATUS *errp,
                             lcb_settings *settings)
 {
@@ -351,28 +413,36 @@ lcbio_pSSLCTX lcbio_ssl_new(const char *tsfile, const char *cafile, const char *
     }
 #endif
 
-    if (cafile || tsfile) {
+    if (tsfile) {
         lcb_log(LOGARGS_S(settings, LCB_LOG_DEBUG), "Load verify locations from \"%s\"", tsfile ? tsfile : cafile);
         if (!SSL_CTX_load_verify_locations(ret->ctx, tsfile ? tsfile : cafile, NULL)) {
             *errp = LCB_ERR_SSL_ERROR;
             goto GT_ERR;
         }
-        if (cafile && keyfile) {
-            lcb_log(LOGARGS_S(settings, LCB_LOG_DEBUG), "Authenticate with key \"%s\", cert \"%s\"", keyfile, cafile);
-            if (!SSL_CTX_use_certificate_chain_file(ret->ctx, cafile)) {
-                *errp = LCB_ERR_SSL_ERROR;
-                goto GT_ERR;
-            }
-            if (!SSL_CTX_use_PrivateKey_file(ret->ctx, keyfile, SSL_FILETYPE_PEM)) {
-                lcb_log(LOGARGS_S(settings, LCB_LOG_ERROR), "Unable to load private key \"%s\"", keyfile);
-                *errp = LCB_ERR_SSL_ERROR;
-                goto GT_ERR;
-            }
-            if (!SSL_CTX_check_private_key(ret->ctx)) {
-                lcb_log(LOGARGS_S(settings, LCB_LOG_ERROR), "Unable to verify private key \"%s\"", keyfile);
-                *errp = LCB_ERR_SSL_ERROR;
-                goto GT_ERR;
-            }
+    } else {
+        // add the capella Root CA if no other CA was specified.
+        lcb_log(LOGARGS_S(settings, LCB_LOG_DEBUG), "Use default CA for TLS verify");
+        *errp = add_certificate_authority(settings, ret->ctx, capella_ca_cert, strlen(capella_ca_cert));
+        if (*errp != LCB_SUCCESS) {
+            goto GT_ERR;
+        }
+    }
+
+    if (cafile && keyfile) {
+        lcb_log(LOGARGS_S(settings, LCB_LOG_DEBUG), "Authenticate with key \"%s\", cert \"%s\"", keyfile, cafile);
+        if (!SSL_CTX_use_certificate_chain_file(ret->ctx, cafile)) {
+            *errp = LCB_ERR_SSL_ERROR;
+            goto GT_ERR;
+        }
+        if (!SSL_CTX_use_PrivateKey_file(ret->ctx, keyfile, SSL_FILETYPE_PEM)) {
+            lcb_log(LOGARGS_S(settings, LCB_LOG_ERROR), "Unable to load private key \"%s\"", keyfile);
+            *errp = LCB_ERR_SSL_ERROR;
+            goto GT_ERR;
+        }
+        if (!SSL_CTX_check_private_key(ret->ctx)) {
+            lcb_log(LOGARGS_S(settings, LCB_LOG_ERROR), "Unable to verify private key \"%s\"", keyfile);
+            *errp = LCB_ERR_SSL_ERROR;
+            goto GT_ERR;
         }
     }
 
